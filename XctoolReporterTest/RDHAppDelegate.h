d132510562b176e1481a3d4f29d865dc1bb07bf3
Rich Hodgkins <rhodgkins@gmail.com>
2014-04-13 19:35:59 +0100

    First code commit

---
+++ XctoolReporterTest/RDHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RDHAppDelegate.h
+//  XctoolReporterTest
+//
+//  Created by Richard Hodgkins on 13/04/2014.
+//  Copyright (c) 2014 Rich H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XctoolReporterTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XctoolReporterTest
+//
+//  Created by Richard Hodgkins on 13/04/2014.
+//  Copyright (c) 2014 Rich H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RDHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RDHAppDelegate class]));
+    }
+}
